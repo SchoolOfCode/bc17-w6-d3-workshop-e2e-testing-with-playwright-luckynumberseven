000ccaaad5227ba040f4ff917f46842409fe527f
Marco Di Summa <mdisumma@yahoo.com>
2024-08-08 12:24:14 +0100

    playright first test

--- tests/todos.test.js
+++ tests/todos.test.js
@@ -2,5 +2,5 @@ import { test, expect } from "@playwright/test";
 
 test("practice test should pass", async ({ page }) => {
   // Barebones test - just open a page
-  await page.goto("http://localhost:3000");
+  await page.goto("http://localhost:3000/");
 });
